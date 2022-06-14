0b54ff407a303bbbfebd2b68464a42a4b55e8868
montelig <g.montelimard43@gmail.com>
2022-06-14 14:41:10 +0200

    help

--- src/view/MyDrawingPanel.cpp
+++ src/view/MyDrawingPanel.cpp
@@ -76,12 +76,7 @@ void MyDrawingPanel::OnPaint(wxPaintEvent &event)
 	//dc.setPen(wxNullPen);
 	//dc.setPen(wxPen(wxcolour(RED)));
 	//dc.DrawLine(m_mousePoint, m_onePoint) ;
-<<<<<<< HEAD
-
-	//dc.DrawLine(m_mousePoint, m_onePoint) ;
-	//dc.DrawRectangle(wxPoint(m_onePoint.x-radius/2, m_onePoint.y-radius/2), wxSize(radius,radius)) ;
-	//dc.DrawCircle(wxPoint(m_mousePoint), radius/2) ;
-=======
+	
 	dc.DrawLine(120,20,220,120);
 	dc.DrawLine(m_mousePoint, m_onePoint) ;
 	dc.DrawRectangle(wxPoint(m_onePoint.x-radius/2, m_onePoint.y-radius/2), wxSize(radius,radius)) ;
