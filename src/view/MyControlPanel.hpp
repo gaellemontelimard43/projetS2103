0e46dd65fd4d91964fd7cbc38afa85c4c8c67f94
Baptiste380 <baptiste38.nicolet@orange.fr>
2022-06-14 10:55:15 +0200

    Version v3 couleurs

--- src/view/MyControlPanel.hpp
+++ src/view/MyControlPanel.hpp
@@ -12,6 +12,8 @@
 #include <wx/image.h>
 #include <wx/file.h>
 #include <wx/bitmap.h>
+#include <iostream>
+#include <string>
 
 
 
@@ -23,17 +25,19 @@ public:
 	MyControlPanel( wxWindow *parent ) ;
 	int GetSliderValue() {return m_slider->GetValue() ;} ;
 	bool GetCheckBoxValue() {return m_checkBox->GetValue() ;} ;
-	string colour[4] = {"Blue", "Red", "Orange", "Yellow"};
+	const char* colour[4] = {"Blue", "Red", "Orange", "Yellow"};
 
 private:
 	void OnButton(wxCommandEvent &event) ;
 	void OnSlider(wxScrollEvent &event) ;
 	void OnCheckBox(wxCommandEvent &event) ;
 	void OnRadioButton(wxCommandEvent &event) ;
+	void OnComboBox(wxCommandEvent &event) ;
 	wxButton* m_button ;
 	wxSlider* m_slider ;
 	wxCheckBox* m_checkBox ;
 	wxRadioButton* m_radioButton;
 	wxRadioButton* m_radioButton2;
+	wxComboBox* m_comboBox ;
 };
 #endif
