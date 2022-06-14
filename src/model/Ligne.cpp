6ab283b55c21c17eb34f51db1f768a68f0c18c97
Baptiste380 <baptiste38.nicolet@orange.fr>
2022-06-14 15:49:22 +0200

    16h00

---
+++ src/model/Ligne.cpp
@@ -0,0 +1,17 @@
+#include "ligne.h"
+#include <iostream>
+using namespace std;
+
+//-------------------------------------------
+Ligne::Ligne(int m_x,int m_y, int m_a, int m_b)
+//-------------------------------------------
+m_x(m_x),m_y(m_y),m_a(m_a),m_b(m_b);
+{   
+}
+
+Ligne::Ligne(){}
+
+Ligne::Ligne(wxPoint p1, wxPoint p2)
+{
+}
+
--- src/model/Ligne.h
+++ src/model/Ligne.h
@@ -1,5 +1,5 @@
-#ifndef __POINT_H__
-#define __POINT_H__
+#ifndef __LIGNE_H__
+#define __LIGNE_H__
 
 #include <string>
 
@@ -12,32 +12,15 @@ class Ligne : public Forme
 {
 public:                        // interface publique de la classe
     Ligne();
-    Ligne(int m_x,int m_y, int a, int b);
-    Ligne(const Point& p1,Point p2);
+    Ligne(int m_x,int m_y, int m_a, int m_b);
+    Ligne(wxPoint p1, wxPoint p2);
+    ~Ligne();
 
-    
+private: 
+    int m_x ; 
+    int m_y ;
+    int m_a ;
+    int m_b ;
+}
 
-    // Setters
-    void SetX(int x);
-    void SetY(int y);
-
-    // Getters
-    int GetX() const;
-    int GetY() const;
-
-    // M�thodes const (qui ne modifie pas l'objet)
-    void Display() const;      // affiche les coordonn�es du point
-    bool XEgalY() const;
-    float Distance(const Point &p) const;
-    float Distance(const Point *p) const;
-
-    // M�thode statique
-    static int GetPointCount(); // retourne le nombre de point
-
-private:   
-    Point p1 ;                     // invisible � l?utilisateur de la classe
-    int m_x;                    // les coordonn�es
-    int m_y;                    // du point
-};
-
-#endif // __POINT_H__ : retour � la ligne obligatoire pour certains compilateurs
+#endif 
